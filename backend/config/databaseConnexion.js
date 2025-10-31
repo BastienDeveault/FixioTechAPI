2912af5cab181cf103c7f85827ae48cbf85133e1
RTS-FleurDeLys <rtsfleurdelys@gmail.com>
2025-10-31 11:27:53 -0400

    [Upd] Changement du fichier databaseConnexion pour render.

--- backend/config/databaseConnexion.js
+++ backend/config/databaseConnexion.js
@@ -6,12 +6,12 @@ const ssl =
     : undefined;
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST || "127.0.0.1",
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "",
-  ssl,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT ?? 3306),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: true },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
